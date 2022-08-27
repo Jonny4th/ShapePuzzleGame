0444e6c79807e25b7a0570ead959de6df07b0bf1
Jonny4th <91357144+Jonny4th@users.noreply.github.com>
2022-08-27 23:03:29 +0700

    Basic Visuals, Panel states system

--- Assets/Scripts/BlockTranslation.cs
+++ Assets/Scripts/BlockTranslation.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class BlockTranslation : MonoBehaviour
 {
     [SerializeField] private float GridSize;
+
+    private void SnapToGrid(Vector3 position)
+    {
+        
+    }
 }
---
+++ Assets/Scripts/PanelOnOff.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class PanelOnOff : MonoBehaviour
+{
+    public bool panelOn;
+    public bool blockOn;
+    [SerializeField] private Material activeMaterial;
+    [SerializeField] private Material inactiveMaterial;
+    [SerializeField] private Material shadowMaterial;
+    [SerializeField] private Material correctMaterial;
+
+    private void Update()
+    {
+        CheckPanelState();
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (blockOn == true) blockOn = false;
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        if (blockOn == false) blockOn = true;
+    }
+
+
+    private void CheckPanelState()
+    {
+        //compound bools
+        bool correct = blockOn && panelOn;
+        bool needFill= !blockOn && panelOn;
+        bool shadow  = blockOn && !panelOn;
+        bool normal  = !blockOn && !panelOn;
+
+        if (needFill)
+        {
+            gameObject.GetComponent<MeshRenderer>().material = activeMaterial;
+        }
+        else if (normal)
+        {
+            gameObject.GetComponent<MeshRenderer>().material = inactiveMaterial;
+        }
+        else if (shadow)
+        {
+            gameObject.GetComponent<MeshRenderer>().material = shadowMaterial;
+        }
+        else if (correct)
+        {
+            gameObject.GetComponent<MeshRenderer>().material = correctMaterial;
+        }
+    }
+}
